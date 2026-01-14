d19a4e1bd4a00592cf1808e915c29cae48676c3c
khushbu <khushbu9625@gmail.com>
2026-01-14 23:33:00 +0530

    //

--- src/routes/-components/navbar/nav-menu.tsx
+++ src/routes/-components/navbar/nav-menu.tsx
@@ -18,7 +18,7 @@ export const NavMenu = (props: ComponentProps<typeof NavigationMenu>) => (
         <NavigationMenuList className="gap-1 space-x-0 text-sm">
             <NavigationMenuItem>
                 <Button variant="ghost" asChild>
-                    <Link to=".">Home</Link>
+                    <Link to="/">Home</Link>
                 </Button>
             </NavigationMenuItem>
             <NavigationMenuItem>
--- src/routes/-components/navbar/navbar.tsx
+++ src/routes/-components/navbar/navbar.tsx
@@ -4,13 +4,16 @@ import { NavMenu } from "./nav-menu"
 import { NavigationSheet } from "./navigation-sheet"
 import { ArrowUpRight } from "lucide-react"
 import { WHATSAPP_NUMBER } from "@/lib/constants"
+import { Link } from "@tanstack/react-router"
 
 const Navbar = () => {
     return (
         <nav className="h-16 bg-background border-b fixed w-full top-0 z-50">
-            <div className="h-full flex items-center justify-between max-w-(--breakpoint-lg) mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="h-full flex items-center justify-between max-w-(--breakpoint-2xl) mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center gap-8">
-                    <Logo />
+                    <Link to="/">
+                        <Logo />
+                    </Link>
                     <NavMenu className="hidden md:block" />
                 </div>
 
